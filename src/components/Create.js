62c9932effee117924f4a829692c27ac55821eb4
Jacqui <jt.cope1@gmail.com>
2023-03-29 17:08:14 +0700

    sets up create component and styling

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 
-const FormExampleForm = () => (
-  <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder="First Name" />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder="Last Name" />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label="I agree to the Terms and Conditions" />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
-);
+export default function Create() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
 
-export default FormExampleForm;
+  const postData = () => {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(checkbox);
+  };
+
+  return (
+    <Form className="create-form">
+      <Form.Field>
+        <label>First Name</label>
+        <input
+          placeholder="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Last Name</label>
+        <input
+          placeholder="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox
+          label="I agree to the Terms and Conditions"
+          onChange={(e) => setCheckbox(!checkbox)}
+        />
+      </Form.Field>
+      <Button type="submit" onClick={postData}>
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+// export default FormExampleForm;
