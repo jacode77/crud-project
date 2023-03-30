c8f7a5a2556312568894652ce0b6d88b7ad67a46
Jacqui <jt.cope1@gmail.com>
2023-03-30 10:04:50 +0700

    Creates form template to facilitate update fctn

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import axios from "axios";
 
 export default function Update() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+
   return (
     <div>
-      <h3>Update Component</h3>
+      <Form className="create-from">
+        <Form.Field>
+          <label>First Name</label>
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>First Name</label>
+          <input
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            label="I agree to the Terms and Conditions"
+            onChange={(e) => setCheckbox(!checkbox)}
+          />
+        </Form.Field>
+        <Button type="submit">Update</Button>
+      </Form>
     </div>
   );
 }
