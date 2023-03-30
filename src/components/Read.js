fbf1ff0acdc6174a7cb087d48c64ea51a2dc3f85
Jacqui <jt.cope1@gmail.com>
2023-03-30 09:55:32 +0700

    Adds update button &redirect link

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Table } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Table, Button } from "semantic-ui-react";
 
 export default function Read() {
   const [APIData, setAPIData] = useState([]);
@@ -13,7 +14,15 @@ export default function Read() {
         setAPIData(response.data);
       });
   }, []);
-  console.log(APIData.firstName);
+
+  const setData = (APIData) => {
+    let { id, firstName, lastName, checkbox } = APIData;
+    localStorage.setItem("ID", id);
+    localStorage.setItem("First Name", firstName);
+    localStorage.setItem("Last Name", lastName);
+    localStorage.setItem("Checkbox Value", checkbox);
+  };
+
   return (
     <div>
       <h3>Read Component</h3>
@@ -23,18 +32,24 @@ export default function Read() {
             <Table.HeaderCell>First Name</Table.HeaderCell>
             <Table.HeaderCell>Last Name</Table.HeaderCell>
             <Table.HeaderCell>Checked</Table.HeaderCell>
+            <Table.HeaderCell>Update</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          {APIData.map((usersData) => {
+          {APIData.map((userData, idx) => {
             return (
-              <Table.Row>
-                <Table.Cell>{usersData.firstName}</Table.Cell>
-                <Table.Cell>{usersData.lastName}</Table.Cell>
+              <Table.Row key={idx}>
+                <Table.Cell>{userData.firstName}</Table.Cell>
+                <Table.Cell>{userData.lastName}</Table.Cell>
                 <Table.Cell>
-                  {usersData.checkbox ? "Checked" : "Unchecked"}
+                  {userData.checkbox ? "Checked" : "Unchecked"}
                 </Table.Cell>
+                <Link to="/update">
+                  <Table.Cell>
+                    <Button onClick={() => setData(APIData)}>Update</Button>
+                  </Table.Cell>
+                </Link>
               </Table.Row>
             );
           })}
