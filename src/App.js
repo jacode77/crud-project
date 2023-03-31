9424a2ab1d69a715218f784739f667bd44624e9a
Jacqui <jt.cope1@gmail.com>
2023-03-31 10:56:58 +0700

    Sets default url Adds navigation to main page if page doesn't exist

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Create from "./components/Create";
 import Read from "./components/Read";
 import Update from "./components/Update";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
         <h1 className="main-header">React Crud Operations</h1>
         <div>
           <Routes>
+            <Route path="/" element={<Navigate to="/read" />} />
             <Route exact path="/create" Component={Create} />
             <Route exact path="/read" Component={Read} />
             <Route exact path="/update" Component={Update} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
       </div>
